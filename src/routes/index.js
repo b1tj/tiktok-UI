3aa3b24bf212fb88aff2ea0075026e8583c89d39
Huy Hoang Le <hhoangle2323@gmail.com>
2023-03-09 14:56:47 +0700

    build default layout

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layouts';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+// Public Route
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+// Private Route
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
